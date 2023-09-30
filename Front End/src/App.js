38cc68c396545d94a3db4cff540f028cb4ecd42f
TheNetherWatcher <117659673+TheNetherWatcher@users.noreply.github.com>
2023-09-30 10:25:21 +0530

    All the work done till now in bharat branch added

---
+++ Front End/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <section className="home">
+      </section>
+      <div style={{ height: '1000px' }}>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ Front End/src/components/Navbar.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import './Navbar.css';
+
+function Navbar() {
+  const [scrolling, setScrolling] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolling(true);
+      } else {
+        setScrolling(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <nav className={`nav ${scrolling ? 'affix' : ''}`}>
+      <div className="container">
+        <div className="logo">
+          <a href="#">Your Logo</a>
+        </div>
+        <div id="mainListDiv" className="main_list">
+          <ul className="navlinks">
+            <li><a href="#">About</a></li>
+            <li><a href="#">Portfolio</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </div>
+        <span className="navTrigger">
+          <i></i>
+          <i></i>
+          <i></i>
+        </span>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;

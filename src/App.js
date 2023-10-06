1c9ee7916f548dd224a8c74e4a54793a8f89c42e
deep <deep.yadavluckyxyz@gmail.com>
2023-10-06 19:26:20 +0530

    frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Home from '../src/pages/home'
+import Search from '../src/pages/Search'
+import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom'; 
+import Complaint from './pages/complaint'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router> 
+       
+        <Routes> 
+                <Route exact path='/' element={< Home />}></Route> 
+                <Route exact path='/search' element={< Search />}></Route> 
+                <Route exact path='/complaint' element={< Complaint />}></Route> 
+                
+        </Routes> 
+        
+    </Router> 
   );
 }
 
---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import './footer.css';
+
+export default function Footer() {
+  return (
+    <div className="footer">
+        <hr className='line'/>
+        <div className="wer">
+        <div className="footercontent">
+            <span className="title"><b>Helpline no</b></span><br/>
+            <span className="contenttitle">Lorem ipsum dolor sit. <br/></span>
+            <span className="contenttitle"> Lorem ipsum dolor sit.  <br/></span>
+            <span className="contenttitle">Lorem ipsum dolor sit.  </span>
+          
+        </div>
+        <div className="footercontent">
+            <span className="title"><b>Register complaint</b></span><br/>
+            <span className="contenttitle">Lorem ipsum dolor sit. </span><br/>
+            <span className="contenttitle">Lorem ipsum dolor sit. y<br/></span>
+        </div>
+        <div className="footercontent">
+            <span className="title"><b>Lorem ipsum dolor sit. </b></span><br/>
+            <span className="contenttitle">Lorem ipsum dolor sit. </span><br/>
+            <span className="contenttitle">Lorem ipsum dolor sit. <br/></span>
+        </div>
+
+        </div>
+       
+        
+    </div>
+  )
+}

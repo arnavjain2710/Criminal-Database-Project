9585c27702b809c0c360dc606f603b415e114e7c
TheNetherWatcher <117659673+TheNetherWatcher@users.noreply.github.com>
2023-10-06 18:50:59 +0530

    some minor change

--- Front End/src/App.js
+++ Front End/src/App.js
@@ -9,20 +9,6 @@ function App() {
       <Home />
       </section>
       <div className='crimes-news-section'>
-      {/* <div className="col-md-2 col-sm-6">
-      <a href="https://lostfound.delhipolice.gov.in/" target="_blank">
-        <div className="service-box">
-          <div className="service-icon yellow">
-            <div className="front-content">
-              <img src="assets/img/tattpar/lost-report" alt="Lost Report" />
-              <h3>
-                <span id="MainContent_SpanLostReport">Lost Report</span>
-              </h3>
-            </div>
-          </div>
-        </div>
-      </a>
-    </div> */}
       </div>
       <div className='links-section'>
       </div>
